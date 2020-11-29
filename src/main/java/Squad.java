66d70579e9543ff4833666c5ec0ea94b7c2296ff
KellyEgesa <kelly.egesa@gmail.com>
2020-11-30 01:29:41 +0300

    Added a test to check is the Squad returned all the squads

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+
 public class Squad {
-    public Squad(int maxSize, String name, String squadCause){}
+    private int maxSize;
+    private String squadName;
+    private String squadCause;
+    private static ArrayList<Squad> instances = new ArrayList<>();
+
+    public Squad(int maxSize, String name, String squadCause){
+        this.maxSize = maxSize;
+        this.squadName = name;
+        this.squadCause = squadCause;
+        instances.add(this);
+    }
+
+    public static ArrayList getAllSquads(){
+        return instances;
+    }
 }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -4,12 +4,21 @@ import static org.junit.Assert.*;
 
 public class SquadTest {
 
-    public Squad setUpSquad(){
+    public Squad setUpSquad() throws Exception{
         return new Squad(8, "Marvel", "Alien Evil");
     }
+
     @Test
-    public void squad_InitialisesCorrectly() {
+    public void squad_InitialisesCorrectly() throws Exception{
         Squad testSquad = setUpSquad();
         assertTrue(testSquad instanceof Squad);
+    }
+
+    @Test
+    public void newSquad_hasAllTheSquads() throws Exception{
+        Squad testSquad = setUpSquad();
+        Squad secondTestSquad = new Squad(5, "Justice League", "Lex Luthor");
+        assertTrue(Squad.getAllSquads().contains(testSquad));
+        assertTrue(Squad.getAllSquads().contains(secondTestSquad));
     }
 }
